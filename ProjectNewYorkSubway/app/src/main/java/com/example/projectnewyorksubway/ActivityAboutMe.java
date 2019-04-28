e38ed0d96b2cf55e2435ca8fc681201ba72deb84
-- <madihubbard1@gmail.com>
2019-04-28 15:05:44 -0500

    sprint 5

---
+++ ProjectNewYorkSubway/app/src/main/java/com/example/projectnewyorksubway/ActivityAboutMe.java
@@ -0,0 +1,13 @@
+package com.example.projectnewyorksubway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAboutMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }
+}
--- ProjectNewYorkSubway/app/src/main/java/com/example/projectnewyorksubway/MainActivity.java
+++ ProjectNewYorkSubway/app/src/main/java/com/example/projectnewyorksubway/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.projectnewyorksubway;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -69,7 +72,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-   private void openActivityD() {
+    //for the menu at the top of the screen showing the about page
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater aboutInflater = getMenuInflater();
+        aboutInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    //the onclick for the about page
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+
+            case R.id.item_about: {
+                Intent myIntentAbout = new Intent(this, ActivityAboutMe.class);
+                startActivity(myIntentAbout);
+                return true;
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openActivityD() {
         Intent myIntentD = new Intent(this, ActivityDLine.class);
         startActivity(myIntentD);
     }
